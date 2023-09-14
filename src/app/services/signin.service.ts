63048a41fddb457eaeb91c9b4e32d7bf06112693
Ishaan Jain <ishaanj@Ishaans-MacBook-Air.local>
2023-09-14 19:06:55 -0400

    server changes

--- src/app/services/signin.service.ts
+++ src/app/services/signin.service.ts
@@ -28,7 +28,7 @@ export class SigninService {
   constructor(private http: HttpClient) { }
 
   getMessagesfromDatabase(): Observable<any>{
-    return this.http.get(this.apiUrl + this.room)
+    return this.http.get(this.apiUrl + "/" + this.room)
   }
 
   sendMessagetoDatabse(Obj: any): Observable<any>{
